0cee7e0dbf4d8e4cc267075ba6ffcc17824d4234
Carlos Eugenio Rojas Morales <carlos.rojasmo@sansano.usm.cl>
2020-07-20 23:42:01 -0400

    arreglo css

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
  
-//Bootstrap
-import 'bootstrap/dist/ccs/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hola mundo!</h1>
     </div>
   );
 }
