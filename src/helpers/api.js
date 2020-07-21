8546d012cfd466ba4fbd85d1ac95f4910c86e3d1
Carlos Eugenio Rojas Morales <carlos.rojasmo@sansano.usm.cl>
2020-07-20 23:56:58 -0400

    api

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const endpoints = {
- development: 'https://gestorspfront.herokuapp.com/',
+ development: ' https://gestorsp.herokuapp.com/',
 // development: 'http://127.0.0.1:8000',
 };
 
