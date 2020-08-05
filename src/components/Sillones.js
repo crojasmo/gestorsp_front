4fd7788877a1e2eb49a8ff21ec640719559eead1
César Paulangelo <cesar.paulangelo@sansano.usm.cl>
2020-08-05 15:16:32 -0400

    la tabla se hizo más bonita

--- src/components/Sillones.js
+++ src/components/Sillones.js
@@ -16,32 +16,37 @@ class Sillones extends React.Component{
     getsillones() {
         var lista=[];
         sillonService.viewAll().then(res => {
-            var lista_sillones=res.data;
-            for (var i in lista_sillones){
-                var sillon=lista_sillones[i];
-                var obj={id:sillon.id,numero:sillon.numero_sillon,sala:sillon.numero_sala,activo:sillon.activo,
-                creacion:sillon.fecha_creacion,update:sillon.fecha_update,delete:sillon.fecha_retirado};
-
-                lista.push(obj);
-            }
-            this.setState({sillones:lista});
+            this.setState({ sillones: res.data.map(sillon =>
+                <tr class="table">
+                    <th>{sillon.id}</th>
+                    <td>{sillon.numero_sillon}</td>
+                    <td>{sillon.activo}</td>
+                    <td>{sillon.numero_sala}</td>
+                    <td>{sillon.fecha_update}</td>
+                    <td>{sillon.fecha_creacion}</td>
+                    <td>{sillon.fecha_retirado}</td>
+                </tr>)
             })
+    })
     }
     render() {
-        var id=this.state.sillones.map((sillon)=>
-        <li>{sillon.id}</li>);
-        var numero=this.state.sillones.map((sillon)=>
-        <li>{sillon.numero}</li>);
-        var sala=this.state.sillones.map((sillon)=>
-        <li>{sillon.sala}</li>);
-        var activo=this.state.sillones.map((sillon)=>
-        <li>{sillon.activo}</li>);
-        var creacion=this.state.sillones.map((sillon)=>
-        <li>{sillon.creacion}</li>);
-        var update=this.state.sillones.map((sillon)=>
-        <li>{sillon.update}</li>);
-        return <ul> IDS{id}NUMEROS de sillon{numero}sala{sala}activo{activo}creacion{creacion}update{update} </ul> ;
-         }
+
+        return <table>
+            <thead>
+            <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Número Sillón</th>
+                <th scope="col">Esta activo</th>
+                <th scope="col">Número Sala</th>
+                <th scope="col">Ultima actualizacion</th>
+                <th scope="col">Fecha de creación</th>
+                <th scope="col">Fecha de desactivación</th>
+            </tr>
+            </thead>
+            <tbody>
+                {this.state.sillones}
+                </tbody>
+            </table>}
 }
 
 export default Sillones;
